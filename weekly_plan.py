7dd7e02a1b013db95176f2b575e32ca78684dbd6
Yazin <yazinsai@users.noreply.github.com>
2020-07-02 11:29:23 +0300

    Start week from Sunday, instead of Monday

--- weekly_plan.py
+++ weekly_plan.py
@@ -12,9 +12,9 @@ def custom_strftime(fmt, t):
 
 def next_week():
     today = datetime.date.today()
-    next_monday = today + datetime.timedelta(days=-today.weekday(), weeks=1)
+    next_sunday = today + datetime.timedelta(days=-today.weekday()-1, weeks=1)
     return [
-        custom_strftime("%B {S}, %Y", next_monday + datetime.timedelta(days=i))
+        custom_strftime("%B {S}, %Y", next_sunday + datetime.timedelta(days=i))
         for i in range(0, 7)
     ]
 
@@ -22,26 +22,26 @@ def next_week():
 def generate_template():
     week = next_week()
     template = """
-Week:: [[{monday}]]
+Week:: [[{sunday}]]
 ## Top Weekly Goals
 ## [[Weekly Habits]]
     
 ## Daily Goals
+    Sunday: [[{sunday}]]
     Monday: [[{monday}]]
     Tuesday: [[{tuesday}]]
     Wednesday: [[{wednesday}]]
     Thursday: [[{thursday}]]
     Friday: [[{friday}]]
     Saturday: [[{saturday}]]
-    Sunday: [[{sunday}]]
     """.format(
-        monday=week[0],
-        tuesday=week[1],
-        wednesday=week[2],
-        thursday=week[3],
-        friday=week[4],
-        saturday=week[5],
-        sunday=week[6],
+        sunday=week[0],
+        monday=week[1],
+        tuesday=week[2],
+        wednesday=week[3],
+        thursday=week[4],
+        friday=week[5],
+        saturday=week[6],
     )
     return template
 
